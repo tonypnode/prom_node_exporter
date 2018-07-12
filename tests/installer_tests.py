9faf6d3356e00351cb3af6bb44787b2c5b7886b1
tonypnode <sgtp@me.com>
2018-07-11 22:10:45 -0400

    Add test for checksum, refactor sha test
    Signed-off-by: tonypnode <sgtp@me.com>

--- tests/installer_tests.py
+++ tests/installer_tests.py
@@ -6,27 +6,35 @@ class InstallerTests(unittest.TestCase):
 
     def setUp(self):
         self.good_sha = 'e92a601a5ef4f77cce967266b488a978711dabc527a720bea26505cba426c029'
-        self.bad_sha_1 = 'abcdef'
-        self.bad_sha_2 = 'e92a601a5ef4f77cce967266b488a978711dabc527a720bea26505cba426c021'
+        self.bad_sha_1 = 'e92a601a5ef4f77cce967266b488a978711dabc527a720bea26505cba426c021'
+        self.bad_sha_2 = 'abcdef'
+        self.known_hash = '80ffbe9f24eb18cc9d4b843d6f50d28bd0228edb32f6051e19bbf4c36a286cfe'
+        self.fail_cases = [
+            [self.good_sha, self.bad_sha_1],
+            [self.good_sha, self.bad_sha_2],
+            [None, self.bad_sha_2],
+            [self.good_sha, None],
+            [self.good_sha, 10 ** 63],
+            [self.good_sha, 10 ** 163],
+            [self.good_sha, 10.13],
+            [self.good_sha, ['a', 'b', 'c']],
+            [self.good_sha, "!@#$%^&*\n()\t\b+<>?:;'\"'"],
+        ]
 
     def test_sha_check(self):
-        """Validate SHA check works
+        """Validate SHA check works"""
 
-        Test A sends known good SHAs - should return True
-        Test B sends known BAD SHA - should return False
-        Test C sends seconds known BAD SHA - should return False
-        Test D/E sends None in either position - should return False
-        Test F sends 64 len int - should return false
-        Test G sends special chars - should return false
-
-        """
         self.assertTrue(exporter.validate_sha(sha=self.good_sha, return_hash=self.good_sha))
-        self.assertFalse(exporter.validate_sha(sha=self.good_sha, return_hash=self.bad_sha_1))
-        self.assertFalse(exporter.validate_sha(sha=self.good_sha, return_hash=self.bad_sha_2))
-        self.assertFalse(exporter.validate_sha(sha=None, return_hash=self.bad_sha_2))
-        self.assertFalse(exporter.validate_sha(sha=self.good_sha, return_hash=None))
-        self.assertFalse(exporter.validate_sha(sha=self.good_sha, return_hash=10**63))
-        self.assertFalse(exporter.validate_sha(sha=self.good_sha, return_hash="!@#$%^&*\n()\t\b+<>?:;'\"'"))
+
+        for fail in self.fail_cases:
+            self.assertFalse(exporter.validate_sha(sha=fail[0], return_hash=fail[1]))
+
+    def test_checksum(self):
+        """validate hashing"""
+        self.assertEqual(exporter.get_checksum(file_name='known_hash_file'), self.known_hash)
+
+        for fail in self.fail_cases:
+            self.assertNotEqual(exporter.get_checksum(file_name='known_hash_file'), fail[1])
 
 
 
